9580077af32fecc596a01d560f1b66e537ebed0a
Rafał Strzałkowski <resumc@wp.pl>
2022-04-01 00:40:01 +0200

    optymalizacja jeszcze bardziej + działa szyfrowanie plików binarnych

--- src/main/java/com/example/model/Key.java
+++ src/main/java/com/example/model/Key.java
@@ -1,7 +1,5 @@
 package com.example.model;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 
 public class Key {
 
@@ -10,19 +8,6 @@ public class Key {
     private final byte[] leftBlock;
     private final byte[] rightBlock;
     private final byte[] connectedBlock = new byte[7];
-
-    private final byte[] leftPattern = {
-            57, 49, 41, 33, 25, 17,  9,
-            1, 58, 50, 42, 34, 26, 18,
-            10,  2, 59, 51, 43, 35, 27,
-            19, 11,  3, 60, 52, 44, 36
-    };
-    private final byte[] rightPattern = {
-            63, 55, 47, 39, 31, 23, 15,
-            7, 62, 54, 46, 38, 30, 22,
-            14,  6, 61, 53, 45, 37, 29,
-            21, 13,  5, 28, 20, 12,  4
-    };
 
     private final byte[] pattern56to48 = {
             14, 17, 11, 24,  1,  5,  3,
@@ -46,6 +31,20 @@ public class Key {
     public Key(String hexaKey) {
         byteBlock = TabUtils.hexStringToBytes(hexaKey);
         strKey = hexaKey;
+        byte[] leftPattern = {
+                57, 49, 41, 33, 25, 17, 9,
+                1, 58, 50, 42, 34, 26, 18,
+                10, 2, 59, 51, 43, 35, 27,
+                19, 11, 3, 60, 52, 44, 36
+        };
+
+        byte[] rightPattern = {
+                63, 55, 47, 39, 31, 23, 15,
+                7, 62, 54, 46, 38, 30, 22,
+                14, 6, 61, 53, 45, 37, 29,
+                21, 13, 5, 28, 20, 12, 4
+        };
+
         leftBlock = TabUtils.permutate(leftPattern, byteBlock);
         rightBlock = TabUtils.permutate(rightPattern, byteBlock);
     }
--- src/main/java/com/example/model/TabUtils.java
+++ src/main/java/com/example/model/TabUtils.java
@@ -1,5 +1,8 @@
 package com.example.model;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 public class TabUtils {
 
     public static byte[] getInitialPermutationPattern() {
@@ -14,12 +17,70 @@ public class TabUtils {
                 63, 55, 47, 39, 31, 23, 15, 7};
     }
 
-    public static byte[] permutate(byte[] pattern, byte[] block, int length) {
+    private static final byte[] HEX_ARRAY = "0123456789abcdef".getBytes(StandardCharsets.US_ASCII);
+
+    public static byte[] permutate(byte[] pattern, byte[] block) {
+        byte length = (byte) ((pattern.length-1) / 8 + 1);
         byte[] newBlock = new byte[length];
-        for (int i = 0; i < length; i++) {
-            newBlock[i] = block[pattern[i]-1];
+        for (int i = 0; i < pattern.length; i++) {
+            int value = getBit(block, pattern[i] - 1);
+            setBit(newBlock, i, value);
         }
         return newBlock;
+    }
+
+
+    public static byte getBit(byte[] block, int index) {
+        int bytePos = index / 8;
+        int bitPos = index % 8;
+        byte value = block[bytePos];
+        return (byte) (value>>(8-(bitPos+1)) & 0x0001);
+    }
+
+    public static byte getBit(byte number, int index) {
+        return (byte) (number >> (8 - ((index % 8) + 1)) & 0x0001);
+    }
+
+    public static void setBit(byte[] block, int index, int val) {
+        int bytePos = index / 8;
+        int bitPos = index % 8;
+        byte oldByte = block[bytePos];
+        oldByte = (byte) (((0xFF7F>>bitPos) & oldByte) & 0x00FF);
+        byte newByte = (byte) ((val<<(8-(bitPos+1))) | oldByte);
+        block[bytePos] = newByte;
+    }
+
+    public static byte[] makeBlocks(byte[] data, int length) {
+        int numberOfBytes = (8 * data.length - 1)/length + 1;
+        byte[] result = new byte[numberOfBytes];
+        for (int i = 0; i < numberOfBytes; i++) {
+            for (int j = 0; j < length; j++) {
+                int value = getBit(data, length * i + j);
+                setBit(result,8*i+j,value);
+            }
+        }
+        return result;
+    }
+
+    public static byte[] xor(byte[] first, byte[] second) {
+
+        byte[] result = new byte[second.length];
+
+        for (int i = 0; i < second.length; i++) {
+            result[i] = (byte) (first[i] ^ second[i]);
+        }
+
+        return result;
+    }
+
+    public static byte[] hexStringToBytes(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
     }
 
     public static byte[] stringToBytes(String text) {
@@ -32,26 +93,6 @@ public class TabUtils {
         return result;
     }
 
-    public static byte[] bytesToBits(byte[] block) {
-        byte[] bitBlock = new byte[block.length * 8];
-        String string = "";
-        int counter = 0;
-        for (int i = 0; i < block.length * 8; i++) {
-            if ((i%8) == 0) {
-                String tmp = TabUtils.byteToBinString(block[counter++]);
-                string = "00000000".substring(tmp.length()) + tmp;
-            }
-            if(string.charAt(i % 8) == '0') {
-                bitBlock[i] = 0;
-            } else {
-                bitBlock[i] = 1;
-            }
-
-        }
-        return bitBlock;
-    }
-
-
     public static String byteToBinString(byte byte1) {
         StringBuilder builder = new StringBuilder();
         for ( int j = 0; j < 8; j++ )
@@ -62,49 +103,6 @@ public class TabUtils {
             builder.append(expect1);
         }
         return ( builder.reverse().toString() );
-    }
-
-    public static byte[] byteToBits(byte number) {
-        byte[] bitBlock = new byte[8];
-        String string = "";
-        for (int i = 0; i < 8; i++) {
-            if (i == 0) {
-                String tmp = TabUtils.byteToBinString(number);
-                string = "00000000".substring(tmp.length()) + tmp;
-            }
-            if(string.charAt(i) == '0') {
-                bitBlock[i] = 0;
-            } else {
-                bitBlock[i] = 1;
-            }
-
-        }
-        return bitBlock;
-    }
-
-    public static String hexToAscii(String hexStr) {
-        StringBuilder output = new StringBuilder();
-
-        if(hexStr.length() % 2 == 1) {
-            hexStr += "0";
-        }
-
-        for (int i = 0; i < hexStr.length(); i += 2) {
-            String str = hexStr.substring(i, i + 2);
-            output.append((char) Integer.parseInt(str, 16));
-        }
-
-        return output.toString();
-    }
-
-    public static byte[] hexToBits(String input) {
-        int n = input.length() * 4;
-        StringBuilder inputBuilder = new StringBuilder(Long.toBinaryString(
-                Long.parseUnsignedLong(input, 16)));
-        while (inputBuilder.length() < n)
-            inputBuilder.insert(0, "0");
-        input = inputBuilder.toString();
-        return binStringToBits(input);
     }
 
     public static String bytesToString(byte[] block) {
@@ -119,67 +117,14 @@ public class TabUtils {
 
     }
 
-    public static String bitsToHex(byte[] bits) {
-        StringBuilder hex = new StringBuilder();
-        byte[] tmp = new byte[4];
-        for (int i = 0; i < bits.length / 4; i++) {
-            System.arraycopy(bits, i * 4, tmp, 0, 4);
-            hex.append(Integer.toHexString(bitsToInt(tmp)));
+    public static String bytesToHex(byte[] bytes) {
+        byte[] hexChars = new byte[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
         }
-        return hex.toString();
-
-    }
-
-    public static byte[] bitsToBytes(byte[] bits) {
-        byte[] result = new byte[8];
-        byte[] tmp = new byte[8];
-
-        for(int i = 0; i < 8; i++) {
-            System.arraycopy(bits, i * 8, tmp, 0, 8);
-            String s = TabUtils.bitsToBinString(tmp);
-            byte byte1 = TabUtils.binStringToByte(s);
-            result[i] = byte1;
-        }
-        return result;
-    }
-
-    public static String bitsToBinString(byte[] bits) {
-        StringBuilder s = new StringBuilder();
-        for (byte bit : bits) {
-            if (bit == 0) {
-                s.append("0");
-            } else {
-                s.append("1");
-            }
-
-        }
-        return s.toString();
-    }
-
-    public static byte binStringToByte(String text) {
-        byte b = 0;
-        byte temp = 1;
-
-        if (text.charAt(0) == '1') {
-            temp = -1;
-            for (int i = 7;i > 0; i--) {
-                if (text.charAt(i) == '0') {
-                    b += temp;
-
-                }
-                temp *= 2;
-            }
-            b -= 1;
-        } else {
-            for (int i = 7;i > 0; i--) {
-                if (text.charAt(i) == '1') {
-                    b += temp;
-
-                }
-                temp *= 2;
-            }
-        }
-        return b;
+        return new String(hexChars, StandardCharsets.UTF_8);
 
     }
 
@@ -196,55 +141,4 @@ public class TabUtils {
         return c;
 
     }
-
-    public static String bitsToString(byte[] bits) {
-
-        return bytesToString(bitsToBytes(bits));
-
-//        StringBuilder str = new StringBuilder();
-//        byte[] tmp = new byte[8];
-//        for (int i = 0; i < bits.length / 8; i++) {
-//            System.arraycopy(bits, i * 8, tmp, 0, 8);
-//            str.append(Character.toString(TabUtils.bitsToInt(tmp)));
-//        }
-//        return str.toString();
-
-    }
-
-    public static byte[] binStringToBits(String bin) {
-        byte[] block = new byte[bin.length()];
-        for (int i = 0;i < bin.length(); i++) {
-            if (bin.charAt(i) == '0') {
-                block[i] = 0;
-            } else {
-                block[i] = 1;
-            }
-
-        }
-        return block;
-
-    }
-
-    public static int bitsToInt(byte[] tab) {
-        int result = 0;
-        int temp = 1;
-
-        for (int i = tab.length - 1; i >= 0; i--) {
-            result += tab[i] * temp;
-            temp *= 2;
-        }
-        return result;
-    }
-
-    public static byte[] xor(byte[] first, byte[] second) {
-        byte[] result = new byte[second.length];
-
-        for (int i = 0; i < second.length; i++) {
-            result[i] = (byte) (first[i] ^ second[i]);
-        }
-
-        return result;
-    }
-
-
 }
